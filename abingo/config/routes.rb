a5c97fa5e2dd90bab7e33a924f82990fd905af53
Marty Haught <mghaught@gmail.com>
2010-06-07 12:10:37 -0400

    Removing unused routes

--- abingo/config/routes.rb
+++ abingo/config/routes.rb
@@ -2,8 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   
   map.devise_for :users
   map.resources :flings
-  map.new_fling "/flings/new", :controller => :flings, :action => "new"
-  map.create_fling "/flings", :controller => :flings, :action => "create", :method => :post
   map.fling "/flung/:id", :controller => :flings, :action => "show"
   
   map.dashboard "/dashboard", :controller => :home, :action => :dashboard
